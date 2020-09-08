2d8c5576649febd0f068be56dc1ac5a5424080b6
VCorak <68239365+VCorak@users.noreply.github.com>
2020-09-08 12:22:15 +0200

    Installed Bootstrap and imported it

--- src/App.js
+++ src/App.js
@@ -6,12 +6,22 @@ import "./App.css";
 
 function App() {
   return (
-   
-      <div className="App">
+    <div className="App">
+      <div className="Container">
         <Search />
+        <footer>
+          <small>
+            <a
+              href="https://github.com/VCorak/weather-app-react"
+              target="_blank"
+            >
+              Open-source code
+            </a>
+            , by Valentina Corak
+          </small>
+        </footer>
       </div>
-     
-    
+    </div>
   );
 }
 
